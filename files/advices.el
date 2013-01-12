e32bd6911e13e4b39234ab587bb8e7520e8cea61
Matus Goljer <matus.goljer@gmail.com>
2013-01-12 13:32:37 +0100

    Added advice to prevent me from accidentaly killing scratch buffers. It now asks y-or-n

--- files/advices.el
+++ files/advices.el
@@ -35,3 +35,10 @@
           (progn (forward-char 1)
                  (just-one-space 0)
                  (backward-char 1)))))
+
+(defadvice kill-this-buffer (around warn-if-scratch activate)
+  "Warn me if I want to kill a scratch buffer"
+  (if (string-match-p "\\\\*scratch" (buffer-name (current-buffer)))
+    (when (y-or-n-p "Do you want to kill this scratch buffer?")
+        ad-do-it)
+    ad-do-it))
