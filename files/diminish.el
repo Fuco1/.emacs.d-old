41009cb7ddbd6c199bf70b0d714c1bcc70073052
Matus Goljer <matus.goljer@gmail.com>
2013-02-17 15:57:12 +0100

    Fix projectile diminish to trigger after projectile is loaded

--- files/diminish.el
+++ files/diminish.el
@@ -2,5 +2,7 @@
 (diminish 'yas-minor-mode)
 (diminish 'undo-tree-mode)
 (diminish 'visual-line-mode)
+(eval-after-load 'projectile
+  '(diminish 'projectile-mode "PJ"))
 (eval-after-load "face-remap"
   '(diminish 'buffer-face-mode))
