1d80a41b41cd43660700482b65d10e9bd4e5bdfd
Matus Goljer <matus.goljer@gmail.com>
2013-03-23 21:36:49 +0100

    Added describe stuff for elisp mode

--- files/emacs-lisp-mode.el
+++ files/emacs-lisp-mode.el
@@ -17,21 +17,33 @@
   "Opens a new line if the point is at the closing parens of
 function on `my-emacs-lisp-open-line-list'."
   (interactive)
-  (my-newline)
-  (when (and (save-excursion
-               (forward-char)
-               (backward-sexp)
-               (when (listp (sexp-at-point))
-                 (memq (car (sexp-at-point)) my-emacs-lisp-open-line-list)))
-             (thing-at-point 'sexp)
-             (eq (following-char) ?\)))
-    (newline)
-    (indent-according-to-mode)
-    (previous-line)))
+  (ignore-errors
+    (my-newline)
+    (when (and (save-excursion
+                 (forward-char)
+                 (backward-sexp)
+                 (when (listp (sexp-at-point))
+                   (memq (car (sexp-at-point)) my-emacs-lisp-open-line-list)))
+               (thing-at-point 'sexp)
+               (eq (following-char) ?\)))
+      (newline)
+      (indent-according-to-mode)
+      (previous-line))))
+
+(defun my-describe-thing-in-buffer ()
+  "Display the full documentation of FUNCTION (a symbol) in the help buffer."
+  (interactive)
+  (let ((function (function-called-at-point))
+        (variable (variable-at-point)))
+    (cond
+     ((/= variable 0) (describe-variable variable))
+     (function (describe-function function)))))
 
 (defun my-emacs-lisp-init ()
-  (define-key emacs-lisp-mode-map (kbd "RET") 'my-emacs-lisp-open-line)
-  (define-key emacs-lisp-mode-map (kbd "M-RET") 'open-next-line)
+  (bind-key "RET" 'my-emacs-lisp-open-line emacs-lisp-mode-map)
+  (bind-key "C-M-;" 'clippy-describe-function emacs-lisp-mode-map)
+  (bind-key "C-M-;" 'clippy-describe-function lisp-interaction-mode-map)
+  (bind-key "C-. ." 'my-describe-thing-in-buffer emacs-lisp-mode-map)
   (letcheck-mode t))
 
 (add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-init)
