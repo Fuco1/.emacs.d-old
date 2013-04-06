d8de0cec2350478d312cfa7c4be1e2dd3df83009
Matus Goljer <matus.goljer@gmail.com>
2013-04-06 02:40:50 +0200

    Added ediff setup

--- files/golden-ratio.el
+++ files/golden-ratio.el
@@ -6,3 +6,14 @@
   (golden-ratio-disable)
   ad-do-it
   (golden-ratio-enable))
+
+(defadvice ediff-quit (after golden-ratio-enable activate)
+  (golden-ratio-enable))
+
+(defadvice ediff-buffers (after golden-ratio-disable activate)
+  (golden-ratio-disable)
+  (balance-windows)
+  (enlarge-window -10)
+  (balance-windows)
+  (execute-kbd-macro "?")
+  (execute-kbd-macro "|"))
