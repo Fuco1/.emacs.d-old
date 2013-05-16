634a6bb22e0ebfa2b8d855902e09bb30e9b1db96
Matus Goljer <matus.goljer@gmail.com>
2013-05-17 01:57:27 +0200

    Remove rotate/transpose window. Is duplicated in elwm

--- files/defuns-buffer.el
+++ files/defuns-buffer.el
@@ -50,23 +50,3 @@ Including indent-buffer, which should not be called automatically on save."
       (when (and (not (file-exists-p dir))
                  (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
         (make-directory dir t)))))
-
-;; Borrowed from https://gist.github.com/1415844
-;; Also see http://emacsworld.blogspot.com/2011/12/moving-buffers-between-windows.html
-(require 'cl)
-(defun rotate-left (l)
-  (append  (cdr l) (list (car l))))
-(defun rotate-windows ()
-  (interactive)
-  (let ((start-positions (rotate-left (mapcar 'window-start (window-list))))
-  (buffers (rotate-left (mapcar 'window-buffer (window-list)))))
-    (mapcar* (lambda (window  buffer pos)
-         (set-window-buffer window buffer)
-         (set-window-start window pos))
-       (window-list)
-       buffers
-       start-positions)))
-
-;; TODO: add function to hide function bodies and leave just name and
-;; docstring, useful for quick overview of the package. See
-;; `hs-make-overlay'
