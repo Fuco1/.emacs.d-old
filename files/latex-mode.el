1ad2beea311402d9a13d557c6a3f94fccc7e13b3
Matus Goljer <matus.goljer@gmail.com>
2013-06-08 16:22:30 +0200

    Add latex settings

--- files/latex-mode.el
+++ files/latex-mode.el
@@ -11,6 +11,8 @@
         (require 'smartparens-latex)
         (sp-local-pair 'latex-mode "\\begin{" "\\end{")
         (sp-local-pair 'latex-mode "\\langle" "\\rangle")
+        (sp-local-tag 'latex-mode "\\ba" "\\begin{align*}" "\\end{align*}")
+
         (use-package preview)
         (use-package font-latex)
         (fset 'tex-font-lock-suscript 'ignore)
@@ -29,7 +31,8 @@
           (TeX-command "LaTeX" 'TeX-master-file nil))
         (bind-key "C-M-x" 'my-latex-compile LaTeX-mode-map)
 
-        (defvar my-latex-wrap-choices '("emph"))
+        (defvar my-latex-wrap-choices '("emph"
+                                        "textsc"))
         (defvar my-latex-wrap-history nil)
 
         (defun my-latex-wrap (macro-name)
@@ -45,6 +48,20 @@
               (goto-char b)
               (insert "\\" macro-name "{"))))
         (bind-key "C-c w" 'my-latex-wrap LaTeX-mode-map)
+
+        (defun my-end-of-environment ()
+          (interactive)
+          (LaTeX-mark-environment)
+          (end-of-region))
+
+        (defun my-beginning-of-environment ()
+          (interactive)
+          (LaTeX-mark-environment)
+          (beginning-of-region)
+          (deactivate-mark))
+
+        (bind-key "M-n" 'my-end-of-environment LaTeX-mode-map)
+        (bind-key "M-p" 'my-beginning-of-environment LaTeX-mode-map)
 
         ;; fix italian quote highlight
         (push '("\"<" "\">") font-latex-quote-list)
@@ -75,6 +92,13 @@ starting with \\ and followed by a block of text enclosed in {}."
         (bind-key "C-c d" 'my-latex-remove-command LaTeX-mode-map)
         (bind-key "M-RET" 'LaTeX-insert-item LaTeX-mode-map)
 
+        (defun my-LaTeX-preview-math ()
+          (interactive)
+          (let ((b (save-excursion (while (texmathp) (backward-char 1)) (1- (point))))
+                (e (save-excursion (while (texmathp) (forward-char 1)) (point))))
+            (preview-region b e)))
+        (bind-key "C-<m-key>" 'my-LaTeX-preview-math preview-map)
+
         (defun my-LaTeX-mode-init ()
           (setq TeX-auto-save t)
           (setq TeX-parse-self t)
@@ -90,6 +114,8 @@ starting with \\ and followed by a block of text enclosed in {}."
           (LaTeX-add-environments
            '("derivation" LaTeX-env-label))
           (TeX-add-symbols '("emph" 1))
+
+          (setq fill-column 100000)
 
           (message "LaTeX mode init complete."))
         ;; ACUTeX replaces latex-mode-hook with LaTeX-mode-hook
